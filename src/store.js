09e62c098d348bd1dc19cbcc802dd6444f359981
Dawid Polok <poldave111@gmail.com>
2023-07-11 21:03:46 +0200

    Add WiP

--- src/store.js
+++ src/store.js
@@ -1,10 +1,17 @@
 import { createStore } from 'redux';
 const reducer = (state, action) => {
-  return state;
+  switch(action.type) {
+    case 'SET_FIELD_VALUE':
+      return {}; // 1. użyje poprzednią wartość stanu i ją zdestrukturyzuje; 2. Ustawi nowy values (w tym samym obiekcie, najepierw dstrukturyzacja) - 
+    default: 
+      return state;
+  }
 };
 
 const size = 5; // wykorzystać zmienną size i zrobić tak żeby podmieniła wartość domyślną w CSS
 const  values = Array(size**2).fill(null);
+//Actions
+export const setFieldValue = payload => ({type: "SET_FIELD_VALUE", payload});
 
 const initialState = {
     nextMove: "x",
