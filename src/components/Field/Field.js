bada2728f9899a6137efd551da16001c2d8b2d26
Dawid Polok <poldave111@gmail.com>
2023-07-25 20:59:16 +0200

    Add checking for winner in store

--- src/components/Field/Field.js
+++ src/components/Field/Field.js
@@ -1,13 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux';
 import styles from './Field.module.scss';
-import { setFieldValue } from '../../store';
+import { onFieldClick } from '../../store';
 // import globalStyles from '../../styles/global.module.css'
 // import typographyStyles from '../../styles/typography.module.css';
 
 function Field(props) {
     const whatIsNextMove = useSelector(state => state.nextMove);
     const dispatch = useDispatch();
-    const changeValue = () => dispatch(setFieldValue({value: whatIsNextMove, index: props.index}));
+    const changeValue = () => dispatch(onFieldClick({value: whatIsNextMove, index: props.index}));
     return (
       <div className={`${styles.container} ${styles.box} ${styles.flex}`} onClick={changeValue}>
           <strong className={`${styles.textAccent} fs-600`}>{props.value}</strong>
