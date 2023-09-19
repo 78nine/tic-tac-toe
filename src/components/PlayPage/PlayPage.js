450d84ace2dd452a6b531dd095eef5e177d85c58
Dawid Polok <poldave111@gmail.com>
2023-09-19 20:23:59 +0200

    Add navigate to / if no values selected on the start page

--- src/components/PlayPage/PlayPage.js
+++ src/components/PlayPage/PlayPage.js
@@ -3,9 +3,20 @@ import { useSelector } from "react-redux";
 import NextMove from "../NextMove/NextMove";
 import Modal from "../Modal/Modal";
 import Points from "../Points/Points";
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
+
 function PlayPage() {
     const values = useSelector((state) => state.values);
     const size = useSelector((state) => state.size);
+    const navigate = useNavigate();
+    useEffect(() => {
+        if(!size) {
+            navigate('/');
+        }
+    },[size]);
+
+
     return (
         <>
             <Points />
